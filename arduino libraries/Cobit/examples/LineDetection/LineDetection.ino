9d180c40d9d453b5a46ab8a27c9ac247736b06c0
jalen <354568001@qq.com>
2021-08-25 18:02:13 +0800

    update

--- arduino libraries/Cobit/examples/LineDetection/LineDetection.ino
+++ arduino libraries/Cobit/examples/LineDetection/LineDetection.ino
@@ -10,15 +10,9 @@ void setup(){
 
 /***************************************************************/
 void loop(){
-  if(Car.Left_LineDetection() == 1)
-	  Serial.println("left not detected");
-  else
+  if(Car.Left_LineDetection() == 0)       //The sensor on the left detects a black line.
 	  Serial.println("left detected");
-  
-  if(Car.Right_LineDetection() == 1)
-	  Serial.println("right not detected");
-  else
+  if(Car.Right_LineDetection() == 0)      //The sensor on the right detects a black line.
 	  Serial.println("right detected");
   delay(500);
-  Serial.println("");
 }
